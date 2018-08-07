a98e24c3a697ef7893b12f370498992a6e2d72d4
meredithheller <meredithheller@outlook.com>
2018-08-07 19:05:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -14,11 +14,12 @@ end
 
 def integer(x)
   if x % 3 == 0
-  puts "Fizz"
-elsif x % 5 == 0
-  puts "Buzz"
-elsif x % 15 == 0
-  puts "FizzBuzz"
-else
-  puts "none"
+   puts "Fizz"
+  elsif x % 5 == 0
+   puts "Buzz"
+  elsif x % 15 == 0
+   puts "FizzBuzz"
+  else
+   puts "none"
 end
+end 
