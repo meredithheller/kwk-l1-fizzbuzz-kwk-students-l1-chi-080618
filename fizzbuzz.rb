dec86a67795373f209697e430a812a54bb9f3611
meredithheller <meredithheller@outlook.com>
2018-08-07 19:06:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,16 @@
-puts "What is your number?"
- x = gets.chomp
- x = x.to_i
+# puts "What is your number?"
+# x = gets.chomp
+# x = x.to_i
  
-if x % 3 == 0
-  puts "Fizz"
-elsif x % 5 == 0
-  puts "Buzz"
-elsif x % 15 == 0
-  puts "FizzBuzz"
-else
-  puts "none"
-end
+# if x % 3 == 0
+#   puts "Fizz"
+# elsif x % 5 == 0
+#   puts "Buzz"
+# elsif x % 15 == 0
+#   puts "FizzBuzz"
+# else
+#   puts "none"
+# end
 
 def integer(x)
   if x % 3 == 0
