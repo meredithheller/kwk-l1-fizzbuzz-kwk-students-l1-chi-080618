7ff341ff7a361f773f0d2a5d10f5e5e07f1717ca
meredithheller <meredithheller@outlook.com>
2018-08-07 19:11:30 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,27 @@
+# puts "What is your number?"
+# x = gets.chomp
+# x = x.to_i
+ 
+# if x % 3 == 0
+#   puts "Fizz"
+# elsif x % 5 == 0
+#   puts "Buzz"
+# elsif x % 15 == 0
+#   puts "FizzBuzz"
+# else
+#   puts "none"
+# end
+
+def integer(x)
+  if x % 15 == 0
+   puts "FizzBuzz"
+  elsif x % 3 == 0
+   puts "Fizz"
+  elsif x % 5 == 0
+   puts "Buzz"
+  else
+   puts "none"
+  end
+end
+
+integer(25)
